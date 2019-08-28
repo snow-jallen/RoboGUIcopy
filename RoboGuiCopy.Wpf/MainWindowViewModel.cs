fe2943843328a712c6d350513e6c9f7327216358
Jonathan Allen <jallen@snow.edu>
2019-08-28 16:28:20 -0600

    Pull viewmodel related stuff into MainWindowViewModel, refactor RoboRunner to take delegates for writing to output & error

--- RoboGuiCopy.Wpf/MainWindowViewModel.cs
+++ RoboGuiCopy.Wpf/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Shared;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,71 @@ using System.Threading.Tasks;
 
 namespace RoboGuiCopy.Wpf
 {
-    public class MainWindowViewModel : RobocopyRunnerVM
+    public class MainWindowViewModel : BindableBase
     {
+        public MainWindowViewModel()
+        {
+            var roboRunner = new RobocopyRunnerVM(
+                    (output) => Output += output + "\n",
+                    (error) => Error += error + "\n"
+                );
+
+            JustDoIt = new DelegateCommand(
+                //execute
+                () => roboRunner.RoboRun(Source, Destination, Recursive, Restartable),
+                //can execute
+                () =>
+                {
+                    return (Source != null && Destination != null);
+                }
+                );
+        }
+
+        private string output;
+        private string error;
+        private string source;
+        private string destination;
+
+        public string Source
+        {
+            get => source; set
+            {
+                source = value;
+                JustDoIt.RaiseCanExecuteChanged();
+            }
+        }
+        public string Destination
+        {
+            get => destination; set
+            {
+                destination = value;
+                JustDoIt.RaiseCanExecuteChanged();
+            }
+        }
+        public bool Recursive { get; set; }
+        public bool Restartable { get; set; }
+
+
+        public string Output
+        {
+            get { return output; }
+            set
+            {
+                output = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Error
+        {
+            get { return error; }
+            set
+            {
+                error = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DelegateCommand JustDoIt { get; set; }
     }
 }
---
+++ Shared/RobocopyRunner.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace Shared
+{
+    public class RobocopyRunnerVM
+    {
+        private readonly Action<string> updateOutput;
+        private readonly Action<string> updateError;
+
+        public RobocopyRunnerVM(Action<string> updateOutput, Action<string> updateError)
+        {
+            this.updateOutput = updateOutput;
+            this.updateError = updateError;
+        }
+
+        public string Output { get; private set; }
+        public string Error { get; private set; }
+
+        public void RoboRun(string src, string dest, bool recursive, bool restartable)
+        {
+            //1) Create the process with arguments robocopy {source} {dest} /E(recursive) /ZB(restartable mode)
+            var p = new Process();
+            p.StartInfo.FileName = "roboCopy.exe";
+            p.StartInfo.Arguments = $"{src} {dest} {(recursive ? "/E" : "")} {(restartable ? "/ZB" : "")} ";
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.UseShellExecute = false;
+
+            p.OutputDataReceived += (sender, args) => updateOutput(args.Data);
+            p.ErrorDataReceived += (sender, args) => updateError(args.Data);
+
+            Output = String.Empty;
+            Error = String.Empty;
+
+            p.Start();
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+
+            p.WaitForExit();
+        }
+    }
+}
